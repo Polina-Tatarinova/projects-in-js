5e408316b3a0874357c82c108f6d5fa7aa49f425
Полина <tatarinova.polya2017@yandex.ru>
2026-07-05 04:47:24 +0300

    Steam Punk Calculator Project

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,32 +2,47 @@ const display = document.querySelector(".display");
 const btns = document.querySelectorAll(".btn");
 const arrayBtn = Array.from(btns);
 
+const localStoreData = localStorage.getItem("display");
+if (localStoreData === null) {
+  display.innerText = "0";
+} else{
+    display.innerText = localStoreData;
+}
+
 arrayBtn.map((button) => {
   button.addEventListener("click", (event) => {
-    switch (event.target.innerText) {
-      case "C":
-        display.innerText = "0";
-        break;
-      case "=":
-        try {
-          display.innerText = eval(display.innerText);
-        } catch (error) {
-          display.innerText = "Error";
-        }
-        break;
-      case "±":
-        display.innerText = "-"
-        break;
-      case "%":
-        const percentages = display.innerText + "/100";
-        display.innerText = eval(percentages);
-        break;
-      default:
-        if (display.innerText === "0" && event.target.innerText !== ".") {
-          display.innerText = event.target.innerText;
-        } else {
-          display.innerText += event.target.innerText;
-        }
-    }
+    
+      switch (event.target.innerText) {
+        case "C":
+          display.innerText = "0";
+          localStorage.setItem('display', display.innerText);
+          break;
+        case "=":
+          try {
+            display.innerText = eval(display.innerText);
+            localStorage.setItem("display", display.innerText);
+          } catch (error) {
+            display.innerText = "Error";
+            localStorage.setItem("display", display.innerText);
+          }
+          break;
+        case "±":
+          display.innerText = "-";
+          localStorage.setItem("display", display.innerText);
+          break;
+        case "%":
+          const percentages = display.innerText + "/100";
+          display.innerText = eval(percentages);
+          localStorage.setItem("display", display.innerText);
+          break;
+        default:
+          if (display.innerText === "0" && event.target.innerText !== ".") {
+            display.innerText = event.target.innerText;
+            localStorage.setItem("display", display.innerText);
+          } else {
+            display.innerText += event.target.innerText;
+            localStorage.setItem("display", display.innerText);
+          }
+      }
   });
 });
