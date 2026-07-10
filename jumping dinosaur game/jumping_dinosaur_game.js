9a976961a2355e7218dd4096de1c1c5182a25c80
Полина <tatarinova.polya2017@yandex.ru>
2026-07-10 07:00:48 +0300

    The jumping dinosaur game is over

--- jumping dinosaur game/jumping_dinosaur_game.js
+++ jumping dinosaur game/jumping_dinosaur_game.js
@@ -16,7 +16,7 @@ button.addEventListener("click", () => {
     startAllSounds();
   } else {
     button.textContent = "Включить звук";
-     stopAllSounds();
+    stopAllSounds();
   }
 });
 
@@ -27,49 +27,49 @@ function startAllSounds() {
     setInterval(() => {
       audio1.volume = 0.5;
       audio1.play();
-    }, 5000)
+    }, 5000),
   );
 
   intervals.push(
     setInterval(() => {
       audio2.volume = 0.9;
       audio2.play();
-    }, 17000)
+    }, 17000),
   );
 
   intervals.push(
     setInterval(() => {
       audio3.volume = 0.3;
       audio3.play();
-    }, 10000)
+    }, 10000),
   );
 
   intervals.push(
     setInterval(() => {
       audio4.volume = 0.3;
       audio4.play();
-    }, 9000)
+    }, 9000),
   );
 
   intervals.push(
     setInterval(() => {
       audio5.volume = 1.0;
       audio5.play();
-    }, 6000)
+    }, 6000),
   );
 
   intervals.push(
     setInterval(() => {
       audio6.volume = 0.2;
       audio6.play();
-    }, 9000)
+    }, 9000),
   );
 
   intervals.push(
     setInterval(() => {
       audio7.volume = 0.8;
       audio7.play();
-    }, 20000)
+    }, 20000),
   );
 }
 
@@ -84,10 +84,41 @@ function stopAllSounds() {
 }
 
 // поменять фон
-const body = document.querySelector('body')
+const body = document.querySelector("body");
 const buttonFon = document.querySelector(".buttonFon");
 buttonFon.addEventListener("click", () => {
   body.classList.toggle("buttonFonActive");
 });
 
 //реализация самой игры
+const dino = document.querySelector(".dino");
+const cactus = document.querySelector(".cactus");
+const score = document.querySelector(".score");
+
+let count = 0;
+function jump() {
+  if (!dino.classList.contains("jump")) {
+    dino.classList.add("jump");
+    count += 1;
+    score.textContent = count;
+  }
+  setTimeout(() => {
+    dino.classList.remove("jump");
+  }, 400);
+}
+
+window.addEventListener("click", jump);
+window.addEventListener("keydown", jump);
+
+let isAlive = setInterval(function () {
+  let dinoTop = parseInt(
+    window.getComputedStyle(dino).getPropertyValue('top'),
+  );
+  let cactusLeft = parseInt(
+    window.getComputedStyle(cactus).getPropertyValue("left"),
+  );
+if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+  alert(`GAME OVER!!`);
+  score.textContent = `0`;
+}
+}, 10);
