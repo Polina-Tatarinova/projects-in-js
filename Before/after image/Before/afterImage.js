7d6a9915d8ba6166761cc7d6e10073aa14f855e0
Полина <tatarinova.polya2017@yandex.ru>
2026-07-12 01:01:32 +0300

    feat: add before-after slider for image comparison
    - 4 independent sliders on one page
    - slider controls visibility of second image
    - responsive layout

--- Before/after image/Before/afterImage.js
+++ Before/after image/Before/afterImage.js
@@ -1,6 +1,19 @@
 const rangeBtns = document.querySelectorAll("#rangeBtn");
 
-rangeBtns.forEach((rangeBtn)=>{
-    const imageBlock = rangeBtn.closest('.image-block')
-    
-})
+rangeBtns.forEach((rangeBtn) => {
+  const imageBlock = rangeBtn.closest(".image-block");
+  const imageAfter = imageBlock.querySelector(".image-after");
+  const imgTwo = imageAfter.querySelector(".img");
+
+  imgTwo.style.width = "500px";
+  imgTwo.style.height = "500px";
+
+  function changingImage() {
+    const sliderValue = rangeBtn.value;
+    imageAfter.style.width = sliderValue + "%";
+  }
+
+  rangeBtn.addEventListener("input", changingImage);
+
+  changingImage();
+});
